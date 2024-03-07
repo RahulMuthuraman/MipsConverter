a94391966f3dfc39c56ce5f9990f87040aadc8f5
RahulMuthuraman <142813535+RahulMuthuraman@users.noreply.github.com>
2024-03-07 13:00:28 -0500

    Initial Files for MIPS Converter
    Provides MIPS OP Code and Funct code (if available) when the user provides the MIPS instruction mnemonic

---
+++ MipsConverter.cpp
@@ -0,0 +1,55 @@
+﻿// MipsConverter.cpp : Defines the entry point for the application.
+//
+
+#include "MipsConverter.h"
+
+using namespace std;
+
+int main()
+{
+	
+	string input;
+
+	while (repeat)
+	{
+		cout << "Input Mips Instruction Mnemonic: ";
+		cin >> input;
+		convert(input);
+	}
+}
+
+void convert(string input)
+{
+	for (size_t i = 0; i < 31; i++)
+	{
+		if (input == Inst[i])
+		{
+			found = true;
+			cout << endl << "The OP code for this instruction is " << opcode[i] << " (hex)" << endl;
+
+			if (funct[i] != "-1")
+			{
+				cout << "The funct code for this instruction is " << funct[i] << " (hex)" << endl << endl;
+			}
+			else
+			{
+				cout << "There is no funct code for this instruction" << endl << endl;
+			}
+
+			cout << "Would you like to terminate the program? (Enter Y to terminate, or any other input to rerun the program)" << endl << endl;
+			cin >> repeatInput;
+			
+
+			if (repeatInput == "Y" || repeatInput == "y")
+			{
+				repeat = false;
+			}
+			break;
+		}
+	}
+	if (!found)
+	{
+		cout << "Instruction not found, please try again" << endl;
+	}
+	found = false;
+}
---
+++ MipsConverter.h
@@ -0,0 +1,22 @@
+﻿// MipsConverter.h : Include file for standard system include files,
+// or project specific include files.
+
+#ifndef MIPS
+#define MIPS
+
+#include <iostream>
+using namespace std;
+
+// TODO: Reference additional headers your program requires here.
+
+bool repeat = true;
+string repeatInput;
+bool found = false;
+
+string Inst[31] = {"add", "addi", "addiu","addu", "and","andi", "beq", "bne", "j", "jal", "jr", "lbu", "lhu", "ll", "lui", "lw", "nor", "or", "ori", "slt", "slti", "sltiu", "sltu", "sll", "srl", "sb", "sc", "sh", "sw", "sub", "subu"};
+
+string opcode[31] = { "0","8", "9", "0", "0", "c", "4", "5", "2", "3", "0", "24", "25", "30", "f", "23", "0" , "0", "d", "0", "a", "b", "0", "0" , "0", "28", "38", "29", "2b", "0" , "0"};
+string funct[31] = { "20", "-1", "-1", "21", "24", "-1", "-1", "-1", "-1", "-1", "08","-1","-1" ,"-1" ,"-1", "-1", "27", "25", "-1", "2a", "-1", "-1", "2b", "00", "02","-1" ,"-1" ,"-1" ,"-1", "22", "23" };
+
+void convert(string input);
+#endif
